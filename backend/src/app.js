d7d2986c38f312511559189b4fcd0b998b18f5bd
Aysh Jaiswal <ayushjaiswal452@gmail.com>
2025-04-25 22:56:47 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'))
 app.use('/admin&user',Userroutes) // login and register route of user //
 app.use('/admin&user',refresTokenroute) // refresh token for user and admin //
-app.use('/superadmin',superadminroutes) // super admin login and register //
+app.use('/superadmin',superadminroutes) // super admin login and register   //
 app.use('/superadmin',superadminrefresroute ) // superadmin refresh  
 
 
